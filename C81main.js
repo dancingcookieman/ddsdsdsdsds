af875fd2e3e9fb60c748a69abbaadbc6594c04ee
dancingcookieman <74688350+dancingcookieman@users.noreply.github.com>
2021-02-10 15:51:52 -0800

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,23 @@
+var canvas = document.getElementById("myCanvas")
+var ctx = canvas.getContext("2d")
+
+ctx.beginPath ()
+ctx.strokeStyle = "red"
+ctx.lineWidth = 4;
+ctx.arc(300,200,40,0,2*Math.PI)
+ctx.stroke ()
+
+canvas.addEventListener("mousedown",my_mousedown)
+
+function my_mousedown(e){
+    var mouse_x = e.clientX - canvas.offsetLeft;
+    var mouse_y = e.clientY - canvas.offsetTop;
+    circle (mouse_x,mouse_y)
+}
+function circle(mouse_x,mouse_y){
+    ctx.beginPath ()
+    ctx.strokeStyle = "red"
+    ctx.lineWidth = 4;
+    ctx.arc(mouse_x,mouse_y,40,0,2*Math.PI)
+    ctx.stroke ()
+}
